d3aacd4ddc91353f2b3af597f28b6e6bdd55b626
debzoti <debjyotisarkar929@gmail.com>
2025-06-29 01:11:21 +0530

    subset

--- kunalKuswahaSolutions/recursion/Subset.java
+++ kunalKuswahaSolutions/recursion/Subset.java
@@ -1,13 +1,14 @@
 package kunalKuswahaSolutions.recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Subset {
     public static void main(String[] args) {
         //using recursion to print all subsets of an array
-       int[] arr = {1, 2, 3};
-       System.out.println(printSubsets(arr));
+       int[] arr = {1, 2, 2};
+       System.out.println(printSubsetsWithDuplicates(arr));
     }
 
     static List<List<Integer>> printSubsets(int[] nums){
@@ -36,20 +37,29 @@ public class Subset {
     }
 
     static List<List<Integer>> printSubsetsWithDuplicates(int[] nums){
+        Arrays.sort(nums);
         //outer listg consist the all the subsets 
         List<List<Integer>> outerList = new ArrayList<>();
         outerList.add(new ArrayList<>());
-
-        for (int num : nums) {
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i<nums.length; i++) {
              //pick every integer of the nums array 
-            int number = num;
-
+            int number = nums[i];
+            start = 0; 
             int n = outerList.size();
 
+            //check if duplicate or not 
+            if (i>0 && nums[i]==nums[i-1]) {
+                                                                                              start = end + 1;
+            }
+ 
+            end = outerList.size()-1;
+          
             //traverese and addd the number to the existing subsets 
             //with cloning the previous memebers
-            for (int i = 0; i < n; i++) {
-                List<Integer> innerList = new ArrayList<>(outerList.get(i));
+            for (int j = start; j < n; j++) {
+                List<Integer> innerList = new ArrayList<>(outerList.get(j));
                 innerList.add(number);
                 outerList.add(innerList);
 
