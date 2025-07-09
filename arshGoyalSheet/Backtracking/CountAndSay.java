6c2891af8c82321f8012993f40fc3200b85f59e0
debzoti <debjyotisarkar929@gmail.com>
2025-07-09 23:02:17 +0530

    count and say

--- arshGoyalSheet/Backtracking/CountAndSay.java
+++ arshGoyalSheet/Backtracking/CountAndSay.java
@@ -2,36 +2,36 @@ package arshGoyalSheet.Backtracking;
 
 public class CountAndSay {
     public static void main(String[] args) {
-        String currStr = "";
+        
         int n =5;
-        handler(1, currStr, n);
-        System.out.println(currStr);
+        String ans = handler(n);
+        System.out.println(ans);
+    }
+    static String handler(int input){
+        //base
+       if(input == 1){
+        return "1";
+       }
+        //stack wise
+       String upperCallAns = handler(input-1); //goes upto 1 and startin to return 1
+       return buildLowerAns(upperCallAns); //start buulding with prev calls and pass to
     }
 
-    static void handler(int number, String currStr, int limit){
-        if(number>limit ){
-            return ;
-        }
-
-        for(int i = 0; i < currStr.length(); i++){
-            char ch = currStr.charAt(i);
-
-            int count = occurence(ch,currStr,i);
-            currStr= currStr.substring(0, i) + Integer.toString(count) + currStr.substring(i+1);
-        }
-        handler(number + 1, currStr, limit);
-    }
-
-    static int occurence(char ch, String currString,int index){
-        int count = 1;
-        for (int i = index; i < currString.length(); i++) {
-            if (i>0 && currString.charAt(i)==currString.charAt(i-1)) {
+    static String buildLowerAns(String s){
+        String res = "";
+        int i =0;
+        
+        //loop upto s length to caclulatr th enext ans to pass
+        while(i<s.length()){
+            int count = 1;
+            while(i+1<s.length() && s.charAt(i)==s.charAt(i+1)){
                 count+=1;
-            }else{
-                break;
+                i+=1;
             }
-
+            res += Integer.toString(count) + s.charAt(i);//go to the next character
+            i+=1;
         }
-        return count;
+        return res;
     }
+    
 }
