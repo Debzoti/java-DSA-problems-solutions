5e9b904f4669851901acc24749f274bbe50f0bf2
debzoti <debjyotisarkar929@gmail.com>
2025-08-24 23:50:41 +0530

    stackReverse

--- arshGoyalSheet/Rercursion/StackReverse.java
+++ arshGoyalSheet/Rercursion/StackReverse.java
@@ -3,8 +3,31 @@ package arshGoyalSheet.Rercursion;
 import java.util.Stack;
 
 public class StackReverse {
-     static void reverse(Stack<Integer> St) {
+     static void reverse(Stack<Integer> st) {
         // code here
-        
+        //base condition 
+        if (st.isEmpty()) {
+            return;
+        }
+
+        int temp = st.peek();
+        st.pop();
+        reverse(st);
+
+        insert(st,temp);
+    }
+
+    static void insert(Stack<Integer> st, int temp){
+        //base condition
+        if (st.isEmpty()) {
+            st.push(temp);
+            return;
+        }
+
+        int val = st.peek();
+        st.pop();
+        insert(st, temp);
+
+        st.push(val);
     }
 }
