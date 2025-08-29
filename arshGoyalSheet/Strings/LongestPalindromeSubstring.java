b6e426cb4976ae2f987da9502c727f0c37839c6f
debzoti <debjyotisarkar929@gmail.com>
2025-08-29 18:47:58 +0530

    LongestPalindromeSubstring

---
+++ arshGoyalSheet/Strings/LongestPalindromeSubstring.java
@@ -0,0 +1,48 @@
+package arshGoyalSheet.Strings;
+
+public class LongestPalindromeSubstring {
+
+    //makr it recursive tomorrow
+    public static void main(String[] args) {
+        String str = "babcdcb";
+        System.out.println(longestPalindrome(str));
+    }
+
+    public static  String longestPalindrome(String s) {
+        int start = 0;
+        int maxLen = Integer.MIN_VALUE;
+        int left =0, right =0;
+        //traverse center of  palinndromes
+        for (int center = 0; center < s.length(); center++) {
+            //even plaindromes
+            left =center;
+            right = center + 1;
+            while (left>0 && right<s.length() && s.charAt(left)==s.charAt(right)) {
+                //found a palindrome store an compare
+                if (right - left +1 > maxLen) {
+                    maxLen= right-left+1;
+                    start = left; //storing coz we need to return this
+                }
+                left --;
+                right ++;
+            }
+
+                //odd plaindromes
+                left =center;
+                right = center ;
+                while (left>0 && right<s.length() && s.charAt(left)==s.charAt(right)) {
+                    //found a palindrome store an compare
+                    if (right - left +1 > maxLen) {
+                        maxLen= right-left+1;
+                        start = left; //storing coz we need to return this
+                    }
+                    left --;
+                    right ++;
+                }
+        }
+
+        String res = s.substring(start, start+maxLen );
+        return res;
+    }
+}
+    
