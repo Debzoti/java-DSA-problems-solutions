e95e7e1202009fab0c8aeee389d926c2c69a7677
debzoti <debjyotisarkar929@gmail.com>
2025-08-30 15:06:08 +0530

    ktsymbolgrammer

---
+++ arshGoyalSheet/Rercursion/KthSymbolGrammer.java
@@ -0,0 +1,22 @@
+package arshGoyalSheet.Rercursion;
+
+public class KthSymbolGrammer {
+    public static void main(String[] args) {
+        System.out.println(kthGrammar(4,7));
+    }
+
+    public static int kthGrammar(int n, int k) {
+        //base 
+        if (n == 1 && k==1) {
+            return 0;
+        }
+
+        int mid = ((int)Math.pow(2, n-1 ) / 2);
+
+        if (k <= mid) {
+            return kthGrammar(n-1, k );
+        } else {
+            return 1 - kthGrammar(n -1 , k - mid);
+        }
+    }
+}
