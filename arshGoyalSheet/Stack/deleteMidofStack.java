dad210c40940902d395acc384f52251423d4f89d
debzoti <debjyotisarkar929@gmail.com>
2025-08-30 14:06:30 +0530

    del mid of stack

---
+++ arshGoyalSheet/Stack/deleteMidofStack.java
@@ -0,0 +1,45 @@
+package arshGoyalSheet.Stack;
+
+import java.util.Stack;
+
+public class deleteMidofStack {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 1; i <5; i++) {
+            st.push(i);
+        }
+
+        deleteMid(st);
+        
+        for (int n : st) {
+            System.out.println(n);
+        }
+    }
+    
+    public static void deleteMid(Stack<Integer> s) {
+        // code here
+        if (s.isEmpty()) {
+            return;
+        }
+        int k = (s.size() / 2) +1;
+
+        solve(s,k);
+    }
+
+    public static void solve(Stack<Integer> st, int k){
+        //base 
+        if(!st.isEmpty() && k ==1){
+            st.pop();
+            return;
+        }
+
+        //hyptothris
+        int temp = st.peek();
+        st.pop();
+        solve(st,k-1);
+
+        //induction
+        st.push(temp);
+    }
+}
