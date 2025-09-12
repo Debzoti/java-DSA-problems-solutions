65d448e7e49317f869f9085b1650a0e8911fe491
debzoti <debjyotisarkar929@gmail.com>
2025-09-12 17:12:01 +0530

    min heifght

---
+++ arshGoyalSheet/greedy/MinimizeTheHeigths.java
@@ -0,0 +1,26 @@
+package arshGoyalSheet.greedy;
+
+import java.util.Arrays;
+
+public class MinimizeTheHeigths {
+    public static void main(String[] args) {
+        int[] arr ={1, 5, 8, 10};
+        System.out.println( getMinDiff(arr,2));
+    }
+
+    public static  int getMinDiff(int[] arr, int k) {
+        // code here
+        Arrays.sort(arr);
+        int initDiff =arr[arr.length -1] - arr[0];
+        int diff = initDiff;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = Math.min(arr[0] + k, arr[i + 1] - k);
+            int max = Math.max(arr[i] + k, arr[arr.length - 1] - k);
+            diff = Math.min(diff, max - min);
+        }
+        
+
+        return diff;
+    }
+}
